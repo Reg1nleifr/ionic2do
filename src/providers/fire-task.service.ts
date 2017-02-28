4eb28f85eb4986c305c9f4642149caa6d46d723f
Florian Reiter <flow.reiter@gmail.com>
2017-03-01 00:59:29 +0100

    CouchDB all the way

---
+++ src/providers/fire-task.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { Task } from '../app/entity/task';
+import 'rxjs/add/operator/map';
+import {AngularFire, FirebaseListObservable, AuthMethods, AuthProviders} from 'angularfire2';
+
+@Injectable()
+export class FireTaskService {
+  private tasks: FirebaseListObservable<Task[]>;
+
+  constructor(private _angularFire: AngularFire) {
+    console.log('Hello Task Provider');
+    this.tasks = _angularFire.database.list('/tasks');
+  }
+
+  get fireTasks(): FirebaseListObservable<Task[]> {
+    return this.tasks;
+  }
+
+  addFireTask(task: Task) {
+    this.tasks.push(task);
+  }
+
+  removeFireTask($key: any) {
+    this.tasks.remove($key);
+  }
+
+  updateFireTask($key: any, task: Task) {
+    this.tasks.update($key, task); // Does this work?
+  }
+}
--- src/providers/task.service.ts
+++ src/providers/task.service.ts
@@ -1,30 +1,159 @@
-import { Injectable } from '@angular/core';
-import { Task } from '../app/entity/task';
-import 'rxjs/add/operator/map';
-import {AngularFire, FirebaseListObservable, AuthMethods, AuthProviders} from 'angularfire2';
+/**
+ * Created by Flo on 14/02/2017.
+ */
+import * as PouchDB from 'pouchdb';
+
+import {Injectable} from '@angular/core';
+import {Task} from '../app/entity/task';
+import { TaskStatus } from '../app/enum/TaskStatus';
 
 @Injectable()
-export class FireTaskService {
-  private tasks: FirebaseListObservable<Task[]>;
+export class TaskService {
 
-  constructor(private _angularFire: AngularFire) {
-    console.log('Hello Task Provider');
-    this.tasks = _angularFire.database.list('/tasks');
+  data: any;
+  db: any;
+  remote: any;
+
+  constructor() {
+    this.db = new PouchDB('doit'); // Shouldnt I also create another for users?
+
+    // TODO: Forwarding auf Raspberry Pi - Applikationsserver (Der mitn Kodi!) - done!
+    this.remote = 'http://192.168.0.20:5984/doit';
+
+    let options = {
+      live: true,
+      retry: true,
+      continuous: true
+    };
+    // TODO: Was tun wenn nicht erreichbar?
+    this.db.sync(this.remote, options);
+
   }
 
-  get fireTasks(): FirebaseListObservable<Task[]> {
-    return this.tasks;
+  setTaskStatus(_id: any, status: TaskStatus) {
+    let me = this.db.get(_id);
+    me.status = status;
+    console.log(me[0]);
+    this.db.put(me);
   }
 
-  addFireTask(task: Task) {
-    this.tasks.push(task);
+  getTodos(): Promise<Task> {
+
+    if (this.data) {
+      return Promise.resolve(this.data);
+    }
+
+    return new Promise(resolve => {
+
+      this.db.allDocs({
+
+        include_docs: true
+
+      }).then((result) => {
+
+        this.data = [];
+
+        let docs = result.rows.map((row) => {
+          // Hier is nix drin :(
+          this.data.push(row.doc);
+          console.log(this.data);
+        });
+
+        resolve(this.data);
+
+        this.db.changes({
+          live: true,
+          since: 'now',
+          include_docs: true
+        }).on('change', (change) => {
+          this.handleChange(change);
+        });
+
+      }).catch((error) => {
+
+        console.log(error);
+
+      });
+
+    });
   }
 
-  removeFireTask($key: any) {
-    this.tasks.remove($key);
+  createTodo(todo: Task): void {
+    this.db.post(todo);
   }
 
-  updateFireTask($key: any, task: Task) {
-    this.tasks.update($key, task); // Does this work?
+  updateTodo(todo: Task): void{
+    this.db.put(todo).catch((err) => {
+      console.log(err);
+    });
   }
+
+  deleteTodo(todo: Task): void{
+    this.db.remove(todo).catch((err) => {
+      console.log(err);
+    });
+  }
+
+  handleChange(change){
+
+    let changedDoc = null;
+    let changedIndex = null;
+
+    this.data.forEach((doc, index) => {
+
+      if(doc._id === change.id){
+        changedDoc = doc;
+        changedIndex = index;
+      }
+
+    });
+
+    //A document was deleted
+    if(change.deleted){
+      this.data.splice(changedIndex, 1);
+    }
+    else {
+
+      //A document was updated
+      if(changedDoc){
+        this.data[changedIndex] = change.doc;
+      }
+
+      //A document was added
+      else {
+        this.data.push(change.doc);
+      }
+
+    }
+
+  }
+
+  private onDatabaseChange = (change) => {
+    var index = this.findIndex(this.db, change.id);
+    var data = this.db[index];
+
+    if (change.deleted) {
+      if (data) {
+        this.db.splice(index, 1); // delete
+      }
+    } else {
+      change.doc.Date = new Date(change.doc.Date);
+      if (data && data._id === change.id) {
+        this.db[index] = change.doc; // update
+      } else {
+        this.db.splice(index, 0, change.doc); // insert
+      }
+    }
+  }
+
+// Binary search, the array is by default sorted by _id.
+  private findIndex(array, id) {
+    var low = 0, high = array.length, mid;
+    while (low < high) {
+      mid = (low + high) >>> 1;
+      array[mid]._id < id ? low = mid + 1 : high = mid
+    }
+    return low;
+  }
+
 }
